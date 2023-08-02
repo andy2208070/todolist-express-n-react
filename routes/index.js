cfcb1a04ef8b7373b876f67af16a719e1080e9eb
Wits.AndyKuo <Wits.AndyKuo@wistron.com>
2023-08-02 19:45:29 +0800

    tmep

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,21 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const crypto = require('crypto');
+const router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+const todoList = [
+  {id: 1, title: '1️⃣ for A'},
+  {id: 2, title: '2️⃣ for B'},
+];
+
+router.get('/todo', function(req, res, next) {
+  // res.render('index', { title: 'Express' });
+  res.send(todoList);
+});
+router.post('/todo', async function (req, res, next) {
+  const body = await req.body;
+  const newTodo = {...body, id: crypto.randomUUID()};
+  todoList.push(newTodo);
+  res.send(todoList);
 });
 
 module.exports = router;
