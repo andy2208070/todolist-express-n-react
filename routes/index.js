ec9423b9718d1f9ec62a61b877d86d769e813585
rainingut <keeprainingut@gmail.com>
2023-08-03 01:35:32 +0800

    CRUD

--- routes/index.js
+++ routes/index.js
@@ -15,7 +15,30 @@ router.post('/todo', async function (req, res, next) {
   const body = await req.body;
   const newTodo = {...body, id: crypto.randomUUID()};
   todoList.push(newTodo);
-  res.send(todoList);
+  res.send(newTodo);
+});
+router.patch('/todo/:id', async function (req, res, next) {
+  const body = await req.body;
+  const {id} = req.params;
+  const todoIdx = todoList.findIndex(todo=>todo.id==id);
+  if(todoIdx<0) {
+    res.status(422);
+    res.send(`❌ no such item!`)
+  } else {
+    todoList[todoIdx].title = body.title;
+    res.send(todoList[todoIdx]);
+  }
+});
+router.delete('/todo/:id', async function(req, res, next) {
+  const {id} = req.params;
+  const todoIdx = todoList.findIndex(todo=>todo.id==id);
+  if(todoIdx<0) {
+    res.status(422);
+    res.send(`❌ no such item!`)
+  } else {
+    todoList.splice(todoIdx, 1);
+    res.send(todoList);
+  }
 });
 
 module.exports = router;
